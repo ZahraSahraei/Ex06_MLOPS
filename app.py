687da11a68efef20a0e566f6d23bdc947f69537b
zahra <za.sahraee@gmail.com>
2024-07-18 18:19:34 +0000

    app.py commited

---
+++ app.py
@@ -0,0 +1,10 @@
+
+
+# function with two parameters with python language
+def process_data(num1, num2):
+    sum = num1 + num2
+    print("Sum:",sum)
+
+# function call with two values
+process_data(5, 4)
+
